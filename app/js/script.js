553c8bc9414cffdd811db4c49da942fac07d91a9
Hum Bruno <hsbruno1@gmail.com>
2022-03-08 21:16:07 +0000

    Added desktop styles and JS validation

---
+++ app/js/script.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll("input");
+const validation = document.querySelectorAll(".validation-text");
+const email = document.querySelector("#email");
+const emailValidation = document.querySelector("#validation-email");
+const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  for (let i = 0; i < inputs.length; i++) {
+    if (!inputs[i].value) {
+      inputs[i].classList.add("empty");
+      validation[i].innerHTML = `${inputs[i].placeholder} cannot be empty`;
+    } else {
+      inputs[i].classList.remove("empty");
+      validation[i].innerHTML = "";
+    }
+  }
+
+  if (!email.value.match(pattern)) {
+    emailValidation.innerHTML = "Looks like this is not an email";
+  } else {
+    emailValidation.innerHTML = "";
+  }
+});
